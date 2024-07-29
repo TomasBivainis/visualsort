338dd510f868059a10519050bfa487bf10428d49
Tomas Bivainis <bivainis.tomas@gmail.com>
2024-07-29 15:04:48 +0300

    fixed the efficiency and program speed

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ os.environ["IMAGEIO_FFMPEG_EXE"] = "./ffmpeg"
 video_width = 1280
 video_height = 720
 header_height = 40
-length = 20
+length = 50
 
 column_width = video_width / length
 column_height = ((video_height - header_height) / length)
@@ -18,31 +18,27 @@ duration = 0.2
 fps = 15
 
 nums = []
+clips = []
+
+video_dir = './video/'
 
 for i in range(1, length + 1):
   nums.append(i)
-  
+
 random.shuffle(nums)
 
-img_dir = './current_images/'
-clips = []
-
-def make_frame(t):
+def make_frame():
   img = Image.new('RGB', (video_width, video_height))
 
   draw = ImageDraw.Draw(img)
 
   for i in range(len(nums)):
     draw.rectangle([(i * column_width, video_height - nums[i] * column_height), ((i + 1) * column_width, video_height)], (255, 255, 255))
-  
+
   return asarray(img)
 
 def add_clip():
-  with VideoClip(make_frame, duration=duration) as clip:
-    clip.write_videofile('tmp.mp4', fps=fps)
-    new_clip = VideoFileClip('tmp.mp4')
-    clips.append(new_clip)
-    os.remove('tmp.mp4')
+  clips.append(make_frame())
 
 for i in range(len(nums)):
   for j in range(i + 1, len(nums)):
@@ -52,8 +48,9 @@ for i in range(len(nums)):
       nums[j] = tmp
       add_clip()
 
-final_clip = concatenate_videoclips(clips)
-final_clip.write_videofile('movie.mp4', fps=fps)
+if not os.path.exists(video_dir):
+  os.mkdir(video_dir)
 
-for file in os.listdir(img_dir):
-  print(file)
+final_clip = ImageSequenceClip(clips, fps=fps)
+
+final_clip.write_videofile(video_dir + 'movie.mp4', fps=fps)
