1303abdf3fd18c4b653fa7aabf03165dd481de16
Tomas Bivainis <bivainis.tomas@gmail.com>
2025-02-08 22:58:30 +0200

    reafactored the package structure

---
+++ name/__init__.py
@@ -0,0 +1,31 @@
+from module import *
+
+def compare(a, b, nums):
+  assign_color(a, "green")
+  assign_color(b, "red")
+  return nums[a] > nums[b]
+
+def swap(a, b, nums):
+  tmp = nums[a]
+  nums[a] = nums[b]
+  nums[b] = tmp
+  add_clip(nums)
+  reset_colors()
+  return nums
+
+def render(algorithm):
+  nums = generate_numbers()
+  reset_colors()
+  
+  add_clip(nums)
+  
+  algorithm(nums)
+  
+  go_through(nums)
+
+  if not os.path.exists(video_dir):
+    os.mkdir(video_dir)
+
+  final_clip = ImageSequenceClip(clips, fps=fps)
+
+  final_clip.write_videofile(video_dir + 'movie.mp4', fps=fps)
---
+++ setup.py
@@ -0,0 +1,31 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',                          # Name of your package
+    version='0.1',                              # Package version
+    packages=find_packages(),                   # Automatically find all packages
+    install_requires=[                          # Dependencies
+        'pillow',                                # Example dependency
+        'random',
+        'os',
+        'moviepy',
+        'numpy',
+        'PIL',
+    ],
+    test_suite='tests',                         # Location of tests
+    tests_require=[                             # Test dependencies
+        'pytest',
+    ],
+    author='Tomas Bivainis',
+    author_email='bivainis.tomas@gmail.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),  # Detailed description from README
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_package',  # GitHub or project URL
+    classifiers=[                               # Package classifiers
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
