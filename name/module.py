fe4ed4e9f57d1532234c9cf8c7e3547ee0d59eaf
Tomas Bivainis <bivainis.tomas@gmail.com>
2025-03-04 22:20:54 +0200

    remove unnecessary functions from module.py

--- name/module.py
+++ name/module.py
@@ -44,19 +44,6 @@ def make_frame(nums):
 def add_clip(nums):
   clips.append(make_frame(nums))
 
-def compare(a, b, nums):
-  assign_color(a, "green")
-  assign_color(b, "red")
-  return nums[a] > nums[b]
-
-def swap(a, b, nums):
-  tmp = nums[a]
-  nums[a] = nums[b]
-  nums[b] = tmp
-  add_clip(nums)
-  reset_colors()
-  return nums
-
 def assign_color(x, color):
   global column_colors
   global colors
@@ -89,20 +76,3 @@ def reset_colors():
   global column_colors
   global default_colors
   column_colors = default_colors.copy()
-
-def render(algorithm):
-  nums = generate_numbers()
-  reset_colors()
-  
-  add_clip(nums)
-  
-  algorithm(nums)
-  
-  go_through(nums)
-
-  if not os.path.exists(video_dir):
-    os.mkdir(video_dir)
-
-  final_clip = ImageSequenceClip(clips, fps=fps)
-
-  final_clip.write_videofile(video_dir + 'movie.mp4', fps=fps)
